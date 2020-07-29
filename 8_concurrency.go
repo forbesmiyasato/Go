3ec62fd3d512dc2bd73dee41bcc6a2c3333c8a10
forbesmiyasato <miya5288@pacificu.edu>
2020-07-28 23:29:57 -0700

    modified 08_concurrency with wait groups

--- 8_concurrency.go
+++ 8_concurrency.go
@@ -3,43 +3,37 @@ package main
 import (
 	"fmt"
 	"golang.org/x/tour/tree"
-	"sort"
+	"sync"
 )
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
-func Walk(t *tree.Tree, ch chan int) {
-	queue := make([]*tree.Tree, 0)
-
-	if t != nil {
-		queue = append(queue, t)
+func Walk(t *tree.Tree, ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	if (t != nil) {
+	wg.Add(1)
+	Walk(t.Left, ch, wg)
+	ch <- t.Value
+	wg.Add(1)
+	Walk(t.Right, ch, wg)
 	}
-	
-	for len(queue) != 0 {
-		temp := queue[0]
-		ch <- temp.Value
-		queue = queue[1:]
-		if temp.Left != nil {
-		queue = append(queue, temp.Left)
-		}
-		if temp.Right != nil {
-		queue = append(queue, temp.Right)
-		}
-	}
-	
-	close(ch)
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool {
+func Same(t1, t2 *tree.Tree, wg *sync.WaitGroup) bool {
 	c1 := make(chan int, 10)
 	c2 := make(chan int, 10)
 	a1 := make([]int, 0)
 	a2 := make([]int, 0)
-	go Walk(t2, c1)
-	go Walk(t1, c2)
+	wg.Add(1)
+	go Walk(t2, c1, wg)
+	wg.Add(1)
+	go Walk(t1, c2, wg)
 	
+	wg.Wait()
+	close(c1)
+	close(c2)
 	for i := range c1 {
 		a1 = append(a1, i)
 	}
@@ -47,10 +41,7 @@ func Same(t1, t2 *tree.Tree) bool {
 	for j := range c2 {
 		a2 = append(a2, j)
 	}
-	
-	sort.Ints(a1)
-	sort.Ints(a2)
-	
+
 	for index := range a1 {
 		fmt.Println(a1[index], a2[index])
 		if (a1[index] != a2[index]) {
@@ -63,11 +54,15 @@ func Same(t1, t2 *tree.Tree) bool {
 
 func main() {
 	c := make(chan int, 10)
-	
-	go Walk(tree.New(2), c)
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
+	go Walk(tree.New(2), c, &wg)
+	wg.Wait()
+	close(c)
 	for i := range c {
 		fmt.Println(i)
 	}
-	
-	fmt.Println(Same(tree.New(2), tree.New(1)))
+	fmt.Println(Same(tree.New(3), tree.New(4), &wg))
 }
